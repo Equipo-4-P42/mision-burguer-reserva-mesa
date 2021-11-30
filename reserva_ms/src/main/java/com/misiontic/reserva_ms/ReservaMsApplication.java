b87baf99bc1f576952234ea8cb06ee4fb4cdcdd9
gmrodriguez170 <gmrodriguez170@gmail.com>
2021-11-30 11:48:32 -0500

    primer commit reserva mesa

---
+++ reserva_ms/src/main/java/com/misiontic/reserva_ms/ReservaMsApplication.java
@@ -0,0 +1,13 @@
+package com.misiontic.reserva_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservaMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservaMsApplication.class, args);
+	}
+
+}
